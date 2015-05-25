83d4d1d70622fc49a3ab1dceff24d9c255f944af
avinandan012 <avinandan012@gmail.com>
2015-05-25 14:45:38 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,20 @@
 # modify this class definitionto use generics
 public class LinkedList{
 
+  protected Node start;
+  protected Node end;
+  protected int size;
+
+//constructors
+public LinkedList(){
+  start = null;
+  end = null;
+  size =0;
+}
+
+public boolean isEmpty()
+{
+  return size==0;
+}
+
 }
