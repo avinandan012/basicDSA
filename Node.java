f8cb46a3d051b5586d23885f56db3ad55126c81c
avinandan012 <avinandan012@gmail.com>
2015-05-25 14:37:43 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -4,11 +4,33 @@ public class Node{
  
  protected int data;
  protected Node link;
- 
+// constructors 
 public Node()
 {
    data = 0;
    link = null
+}
+
+public Node(int data,Node link)
+{
+ this.data = data;
+ this.link = link;
+}
+
+
+//setters
+public void setData(int data){
+  this.data = data;
+}
+public void setLink(Node link){
+ this.link = link;
+}
+//getters
+public int getData(){
+ return data;
+}
+public Node getLink(){
+ return link;
 }
 
 }
