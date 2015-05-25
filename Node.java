f803172dae0520856c08bfc58de7feb465af91fe
avinandan012 <avinandan012@gmail.com>
2015-05-25 14:30:21 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,5 +1,14 @@
 # Node.java -- This file is to create a Node in a linked list 
- 
+# this is a basic 
 public class Node{
+ 
+ protected int data;
+ protected Node link;
+ 
+public Node()
+{
+   data = 0;
+   link = null
+}
 
 }
