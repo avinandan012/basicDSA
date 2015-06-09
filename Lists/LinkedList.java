6c03429983d96d48f327aca98a8e8604f7d24979
Avinandan Giri <avinandan012@gmail.com>
2015-06-09 12:31:30 +0530

    Update LinkedList.java

--- Lists/LinkedList.java
+++ Lists/LinkedList.java
@@ -23,8 +23,17 @@ public int getSize(){
 public void insertAtStart(int val){
   Node insertNode = new Node(11);
   size++;
-  
+  if(start == null){
+      start = insertNode;
+      end = start;
+  }
+  else{
+    insertNode.setLink(start);
+    start = insertNoder;
+  }
 }
+
+
 public void insertAtEnd(int val){
   
 }
